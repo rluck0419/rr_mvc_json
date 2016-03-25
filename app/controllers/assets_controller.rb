d6d03b50f76c4bc34cc92c4aac8f266bba849197
Justin Herrick <justin@justinherrick.com>
2016-03-25 00:03:18 -0500

    ¯\_(ツ)_/¯

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -2,7 +2,7 @@ class AssetsController < ApplicationController
   def handle
     file_name = "app/assets/#{params[:type]}/#{params[:name]}"
     if request[:format] == "css" && File.exist?(file_name.gsub('css', 'scss'))
-      data = `sass #{file_name.gsub('css', 'scss')}`
+      data = `sass #{file_name.gsub('css', 'scss')}` # This is so slow
     else
       data = File.read(file_name)
     end
